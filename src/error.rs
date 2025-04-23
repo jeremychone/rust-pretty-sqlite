c7f363e612796dbc837eb9f4e5abb9f6c4b4826f
Jeremy Chone <jeremy.chone@gmail.com>
2025-04-23 09:25:51 -0700

    . error - use derive_more for display

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,9 @@
-use derive_more::From;
+use derive_more::{Display, From};
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug, From)]
+#[derive(Debug, From, Display)]
+#[display("{self:?}")]
 pub enum Error {
 	CantPrintRowsHasNoStatement,
 	SQLiteTextCellIsNotUtf8,
@@ -13,12 +14,6 @@ pub enum Error {
 }
 
 // region:    --- Error Boilerplate
-
-impl core::fmt::Display for Error {
-	fn fmt(&self, fmt: &mut core::fmt::Formatter) -> core::result::Result<(), core::fmt::Error> {
-		write!(fmt, "{self:?}")
-	}
-}
 
 impl std::error::Error for Error {}
 
